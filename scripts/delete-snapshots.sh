f2abf2f81d5e2d4041577a3317424bd568a7dbbf
Tomasz Bekas <tomasz.bekas@gmail.com>
2021-01-25 22:06:05 +0100

    Update the script * adjust jq to version 1.5 * fix warnings * basic validation * usage docs

--- scripts/delete-snapshots.sh
+++ scripts/delete-snapshots.sh
@@ -1,44 +1,120 @@
 #!/usr/bin/env bash
+set -e
 
-# Usage:
-# 1. Set env variable:
-#   export ES_BASE_URL="http://elasticsearch.com:9200"
-# 2. Run script and pass snapshot ids (hashes) as positional arguments:
-#   ./delete-snapshots.sh abc xyz
+# Get the docs with:
+# ./delete-snapshots --help
 
 _ES_BASE_URL="${ES_BASE_URL:-http://localhost:9200}"
+_ES_SNAPSHOTS_INDEX="${ES_SNAPSHOTS_INDEX:-snapshots}"
+_ES_CHECKPOINTS_INDEX="${ES_CHECKPOINTS_INDEX:-checkpoints}"
+_ES_TRANSACTIONS_INDEX="${ES_TRANSACTIONS_INDEX:-transactions}"
 
-snapshots=("$@")
+curl_opts=(--silent --fail)
 
-checkpoints=()
-for snapshot in ${snapshots[*]}
-do
- curr=( $(curl --silent "$_ES_BASE_URL/snapshots/_doc/$snapshot" | jq --raw-output '._source.checkpointBlocks[]') )
- checkpoints=("${checkpoints[@]}" "${curr[@]}")
-done
+function error() {
+  echo "$1" 1>&2
+  exit 1
+}
 
-transactions=()
-for checkpoint in ${checkpoints[*]}
-do
-  curr=( $(curl --silent "$_ES_BASE_URL/checkpoints/_doc/$checkpoint" | jq --raw-output '._source.transactions[]') )
-  transactions=("${transactions[@]}" "${curr[@]}")
-done
+function run() {
 
+  snapshots=("$@")
 
-bulkJsonStr=""
-bulkJsonStr+=$(jq --null-input --compact-output '$ARGS.positional[] | {delete:{_index:"transactions",_id:.}}' --args "${transactions[@]}")$'\n'
-bulkJsonStr+=$(jq --null-input --compact-output '$ARGS.positional[] | {delete:{_index:"checkpoints",_id:.}}' --args "${checkpoints[@]}")$'\n'
-bulkJsonStr+=$(jq --null-input --compact-output '$ARGS.positional[] | {delete:{_index:"snapshots",_id:.}}' --args "${snapshots[@]}")$'\n'
+  checkpoints=()
+  for snapshot in ${snapshots[*]}; do
+    curr=()
+    while IFS='' read -r line; do curr+=("$line"); done < <(curl "${curl_opts[@]}" "$_ES_BASE_URL/$_ES_SNAPSHOTS_INDEX/_doc/$snapshot" | jq --raw-output '._source.checkpointBlocks[]')
+    checkpoints=("${checkpoints[@]}" "${curr[@]}")
+  done
 
-echo "Documents selected for deletion"
-echo "$bulkJsonStr"
+  transactions=()
+  for checkpoint in ${checkpoints[*]}; do
+    curr=()
+    while IFS='' read -r line; do curr+=("$line"); done < <(curl "${curl_opts[@]}" "$_ES_BASE_URL/$_ES_CHECKPOINTS_INDEX/_doc/$checkpoint" | jq --raw-output '._source.transactions[]')
+    transactions=("${transactions[@]}" "${curr[@]}")
+  done
 
-read -r -p "Are you sure you want to delete these documents? [y/N] " response
-if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]
-then
-    result=$(curl --silent -X POST "$_ES_BASE_URL/_bulk" -H "Content-Type: application/json" -d "$bulkJsonStr")
+  function deleteItems() {
+    jq --null-input --compact-output '$ids | split(" ")[] | {delete:{_index:$index,_id:.}}' --arg index "$1" --arg ids "$2"
+  }
+
+  bulkJsonStr=""
+  bulkJsonStr+=$(deleteItems "$_ES_TRANSACTIONS_INDEX" "${transactions[*]}")$'\n'
+  bulkJsonStr+=$(deleteItems "$_ES_CHECKPOINTS_INDEX" "${checkpoints[*]}")$'\n'
+  bulkJsonStr+=$(deleteItems "$_ES_SNAPSHOTS_INDEX" "${snapshots[*]}")$'\n'
+
+  echo "Documents selected for deletion"
+  echo "$bulkJsonStr"
+
+  read -r -p "Are you sure you want to delete these documents? [y/N] " response
+  if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]; then
+    result=$(curl "${curl_opts[@]}" -X POST "$_ES_BASE_URL/_bulk" -H "Content-Type: application/json" -d "$bulkJsonStr")
     echo "Deletion result"
     echo "$result"
-else
+  else
     echo "Canceled"
-fi
+  fi
+}
+
+function validateInput() {
+
+  curl "${curl_opts[@]}" "$_ES_BASE_URL" > /dev/null || error "Unable to connect to: $_ES_BASE_URL"
+
+  function validateIndex() {
+    curl "${curl_opts[@]}" "$_ES_BASE_URL/_cat/indices/$1" >/dev/null || error "Index not found: $1"
+  }
+
+  validateIndex "$_ES_SNAPSHOTS_INDEX"
+  validateIndex "$_ES_CHECKPOINTS_INDEX"
+  validateIndex "$_ES_TRANSACTIONS_INDEX"
+}
+
+positional=()
+while [[ $# -gt 0 ]]; do
+  arg="$1"
+
+  case $arg in
+  -b | --base-url)
+    shift
+    _ES_BASE_URL=$1
+    shift
+    ;;
+  -s | --snapshots-index)
+    shift
+    _ES_SNAPSHOTS_INDEX=$1
+    shift
+    ;;
+  -c | --checkpoints-index)
+    shift
+    _ES_CHECKPOINTS_INDEX=$1
+    shift
+    ;;
+  -t | --transactions-index)
+    shift
+    _ES_TRANSACTIONS_INDEX=$1
+    shift
+    ;;
+  -h | --help)
+    cat <<-EOF
+Usage ./$(basename "$0") [options] <snapshot hashes...>
+
+Options:
+  -b, --base-url <url>                Address of the elasticsearch REST API
+  -s, --snapshots-index <name>        Name of the index containing snapshots (default "snapshots")
+  -c, --checkpoints-index <name>      Name of the index containing checkpoint blocks (default "checkpoints")
+  -t, --transactions-index <name>     Name of the index containing transactions (default "transactions")
+EOF
+    exit 0
+    ;;
+  *)
+    positional+=("$1")
+    shift
+    ;;
+  esac
+done
+
+set -- "${positional[@]}"
+
+validateInput
+
+run "$@"
