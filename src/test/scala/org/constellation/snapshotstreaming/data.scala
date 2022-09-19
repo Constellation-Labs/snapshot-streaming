fcb184c4cca132e2c49cdf775e828e54561b9987
kriscieslak <kcieslak@evojam.com>
2022-09-19 19:58:07 +0200

    Fix and format.

--- src/test/scala/org/constellation/snapshotstreaming/data.scala
+++ src/test/scala/org/constellation/snapshotstreaming/data.scala
@@ -3,6 +3,8 @@ package org.constellation.snapshotstreaming
 import cats.data.{NonEmptyList, NonEmptySet}
 
 import scala.collection.immutable.{SortedMap, SortedSet}
+
+import org.tessellation.dag.snapshot.epoch.EpochProgress
 import org.tessellation.dag.snapshot.{GlobalSnapshot, SnapshotOrdinal}
 import org.tessellation.schema.ID.Id
 import org.tessellation.schema.height.{Height, SubHeight}
@@ -11,9 +13,9 @@ import org.tessellation.security.hash.Hash
 import org.tessellation.security.hex.Hex
 import org.tessellation.security.signature.Signed
 import org.tessellation.security.signature.signature.{Signature, SignatureProof}
+
 import eu.timepit.refined.auto._
 import eu.timepit.refined.types.numeric.NonNegLong
-import org.tessellation.dag.snapshot.epoch.EpochProgress
 
 object data {
 
