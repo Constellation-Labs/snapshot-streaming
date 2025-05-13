dd3ef79836f35e7bc9d6587769b6305eddbc206c
Gabriel Claramunt <gabriel.claramunt@gmail.com>
2025-05-13 14:59:21 -0300

    release for 3.0.0-rc.8

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -20,7 +20,7 @@ object Dependencies {
     val logstash = "7.2"
     val organizeImports = "0.6.0"
     val refined = "0.10.1"
-    val tessellation = "3.0.1-rc.7-6-15e42a33-SNAPSHOT"
+    val tessellation = "3.0.0-rc.8"
     val weaver = "0.8.1"
   }
 
