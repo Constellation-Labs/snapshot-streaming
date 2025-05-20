64a31ee5011efb6baec035b018e07da6820c4f53
Marcus Vinicius Girolneto Sousa <marcus.girolneto@gmail.com>
2025-05-20 13:33:23 -0300

    feat: updating tessellation version

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -18,7 +18,7 @@ object Dependencies {
     val logstash = "7.2"
     val organizeImports = "0.6.0"
     val refined = "0.10.1"
-    val tessellation = "3.0.0-rc.8"
+    val tessellation = "3.0.0"
     val weaver = "0.8.1"
   }
 
