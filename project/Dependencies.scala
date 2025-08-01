b7ef3cbcebce452ff27871bf450634783c6c3551
Marcus Vinicius Girolneto Sousa <marcus.girolneto@gmail.com>
2025-08-01 09:03:16 -0300

    chore: updating tessellation version on streaming (testnet)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -20,7 +20,7 @@ object Dependencies {
     val logstash = "7.2"
     val organizeImports = "0.6.0"
     val refined = "0.10.1"
-    val tessellation = "3.2.1-rc.2-43-21f4f179-SNAPSHOT"
+    val tessellation = "3.2.1-rc.2-47-5065280c-SNAPSHOT"
     val weaver = "0.8.1"
   }
 
