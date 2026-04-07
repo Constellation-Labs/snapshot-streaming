dc8e78ebf3b06808e49ea11a210102d8da09e7c4
Marcus Vinicius Girolneto Sousa <marcus.girolneto@gmail.com>
2026-04-07 12:35:22 -0300

    fix: updating tessellation dependency

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -20,7 +20,7 @@ object Dependencies {
     val logstash = "7.2"
     val organizeImports = "0.6.0"
     val refined = "0.10.1"
-    val tessellation = "3.5.13"
+    val tessellation = "3.5.15"
     val weaver = "0.8.1"
   }
 
